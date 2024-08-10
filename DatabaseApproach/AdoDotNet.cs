359cd325dd9aa1d417ba2fbc6049357fcad45bab
RezaPaidar <reza.paidar@outlook.com>
2024-08-10 18:34:24 +0330

    Add project files.

---
+++ DatabaseApproach/AdoDotNet.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseApproach
+{
+    public class AdoDotNet
+    {
+        private SqlConnection _connection;
+        private const string connectionString = "Data Source=.;Initial Catalog=FIFA;Integrated Security=True;";
+
+
+        public AdoDotNet()
+        {
+            _connection = new SqlConnection(connectionString);
+        }
+
+        private bool Connect()
+        {
+            if (_connection.State != ConnectionState.Open)
+            {
+                _connection.Open();
+                return true;
+            }
+            return false;
+        }
+
+        private void Disconnect()
+        {
+            if (_connection.State == ConnectionState.Open)
+            {
+                _connection.Close();
+            }
+        }
+
+        /// <summary>
+        /// For execute update, delete and insert commands [Write Operations]
+        /// </summary>
+        /// <param name="query"></param>
+        /// <returns></returns>
+        public int ExecuteCommand_To_SQLServer(string query)
+        {
+            try
+            {
+                Connect();
+                var cmd = new SqlCommand();
+                cmd.Connection = _connection;
+                cmd.CommandText = query;
+                cmd.CommandType = CommandType.Text;
+
+                int result = cmd.ExecuteNonQuery();
+
+                return result;
+            }
+            catch (SqlException e)
+            {
+                return e.Number * (-1);
+            }
+            finally
+            {
+                Disconnect();
+            }
+        }
+
+        public string Read_Opr(string query)
+        {
+            try
+            {
+                Connect();
+                var cmd = new SqlCommand();
+                cmd.Connection = _connection;
+                cmd.CommandText = query;
+                cmd.CommandType = CommandType.Text;
+
+                string result = "";
+
+                SqlDataReader reader = cmd.ExecuteReader();
+                if (reader.Read())
+                {
+                    result = reader["cName"].ToString();
+                }
+                return result;
+
+            }
+            catch (SqlException e)
+            {
+                return null;
+            }
+            finally
+            {
+                Disconnect();
+            }
+        }
+    }
+}
---
+++ DatabaseApproach/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseApproach
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //WriteCommand();
+            Read_Data();
+        }
+
+        static void WriteCommand()
+        {
+            Console.WriteLine("Enter new team");
+            var newTeam = Console.ReadLine();
+
+            string query = "";
+
+            var db = new AdoDotNet();
+            int r = db.ExecuteCommand_To_SQLServer(query);
+            if (r > 0)
+            {
+                Console.WriteLine("New team created successfully");
+            }
+            else
+            {
+                Console.WriteLine("Error number: " + r);
+            }
+        }
+
+        static void Read_Data()
+        {
+            var query = $"SELECT CONCAT(c.CFirstName,' ' ,c.CFamilyName) AS cName FROM Coach c INNER JOIN CoachHistory ch ON c.Cid = ch.Cid INNER JOIN Teams t ON ch.Teamid = t.Teamid WHERE ch.Teamid = 1";
+
+            var db = new AdoDotNet();
+            var obj = db.Read_Opr(query);
+            Console.WriteLine(obj.ToString());
+        }
+
+    }
+}
